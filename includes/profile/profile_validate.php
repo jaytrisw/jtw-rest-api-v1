856b7eea6918883ee683399ace3a2bad83000d79
Joshua T. Wood <joshuatw@gmail.com>
2022-11-28 17:38:08 +0100

    add error codes

--- includes/profile/profile_validate.php
+++ includes/profile/profile_validate.php
@@ -12,14 +12,14 @@ function validate_post_callback(WP_REST_Request $request)
             case 'jwt_auth_valid_token':
                 return Response::success(array('token' => $token, 'status' => 'valid_access_token'));
             case 'jwt_auth_no_auth_header':
-                return Response::success(array('token' => null, 'status' => 'no_authoriztion_header'));
+                return Response::success(array('token' => null, 'status' => 'no_authorization_header'));
             case 'jwt_auth_invalid_token':
                 if ($response['message'] == 'Expired token') {
                     return Response::success(array('token' => $token, 'status' => 'expired_access_token'));
                 }
                 return Response::success(array('token' => $token, 'status' => 'invalid_access_token'));
             default:
-                return Response::success(array('token' => $token, 'status' => 'unknown_error'));
+                return Response::success(array('token' => $token, 'status' => 'unknown'));
         }
     });
 }
