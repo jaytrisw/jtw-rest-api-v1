10d12b2baa12270ca034d18f1f6e8a94709788fa
Joshua T. Wood <joshuatw@gmail.com>
2022-11-29 01:34:10 +0100

    update unknown backed value

--- includes/response.php
+++ includes/response.php
@@ -44,13 +44,13 @@ enum ErrorCode: int {
 
 enum StatusCode: int {
 
+    case UNKNOWN = 499;
     case BAD_REQUEST = 400;
     case UNAUTHORIZED = 401;
     case PAYMENT_REQUIRED = 402;
     case FORBIDDEN = 403;
     case NOT_FOUND = 404;
     case METHOD_NOT_ALLOWED = 405;
-    case UNKNOWN = 405;
     case CONFLICT = 409;
 
 }
