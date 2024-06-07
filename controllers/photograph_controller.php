6707708b238004c2b9fcce252357970613442952
Joshua T. Wood <joshuatw@gmail.com>
2024-06-07 11:35:45 +0200

    update photographs endpoint

--- controllers/photograph_controller.php
+++ controllers/photograph_controller.php
@@ -1,18 +1,24 @@
 <?php
 
-class PhotographsController {
+class PhotographsController
+{
+    private PhotographDataSource $data_source;
 
-	private PhotographDataSource $data_source;
+    public function __construct(PhotographDataSource $data_source)
+    {
+        $this->data_source = $data_source;
+    }
 
-	public function __construct(PhotographDataSource $data_source) {
-		$this->data_source = $data_source;
-	}
+    public function get_photograph(int $identifier): Photograph
+    {
+        return $this->data_source->get_photograph($identifier);
+    }
 
-	public function get_photograph(int $identifier): Photograph {
-		return $this->data_source->get_photograph($identifier);
-	}
-	
-	public function get_photographs(): array {
-	    return $this->data_source->get_photographs();
-	}
+    public function get_photographs(
+        int $count,
+        int $page,
+        string $search
+    ): array {
+        return $this->data_source->get_photographs($count, $page, $search);
+    }
 }
